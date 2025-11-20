265eb380f88f22e420320d59dfe70229d109bfbf
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-20 15:41:10 +0530

    Create Player.java

---
+++ server/src/main/java/com/mygame/server/Player.java
@@ -0,0 +1,19 @@
+package com.mygame.server;
+
+public class Player {
+    private String name;
+    private String id;
+
+    public Player(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
