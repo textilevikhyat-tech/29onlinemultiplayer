3c3017b5c701c03d03a982d4c7b28e0de80cce1f
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-23 17:03:04 +0530

    Update Constants.java

--- app/src/main/java/com/shaheed/online29/Constants.java
+++ app/src/main/java/com/shaheed/online29/Constants.java
@@ -1,19 +1,6 @@
-package com.shaheed.online29;
-
-import android.app.Activity;
-
-/**
- * Created by Shaheed on 11/17/2014.
- */
 public class Constants {
 
-    public static final String URL_REGISTRATION = "http://10.0.3.2:8080/Online29/RegistrationServlet";
-    public static final String URL_LOGIN = "http://10.0.3.2:8080/Online29/LoginServlet";
-    public static final String URL_CREATE = "http://10.0.3.2:8080/Online29/CreateGameServlet";
-    public static final String URL_GAMELIST = "http://10.0.3.2:8080/Online29/GameList";
-    public static final String URL_JOINGAME = "http://10.0.3.2:8080/Online29/JoinGameServlet";
-
-    public static final String METHOD_GET = "GET";
+    public static final String WS_URL = "wss://two9onlinemultiplayer.onrender.com/ws/game";
 
     public static String username;
     public static int actionUserId = 0;
@@ -21,10 +8,5 @@ public class Constants {
     public static Boolean hostingGame = false;
     public static Boolean joiningGame = false;
     public static String gameId = "0";
-
-    public static void quitButton(Activity activity){
-        activity.finish();
-        System.exit(0);
-    }
 
 }
