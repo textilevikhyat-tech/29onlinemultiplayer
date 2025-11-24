aa914eb0bb6edbbee81a82e6d06e90aa29fe5d2e
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-24 11:40:30 +0530

    Update GameActivity.java

--- app/src/main/java/com/shaheed/online29/GameActivity.java
+++ app/src/main/java/com/shaheed/online29/GameActivity.java
@@ -3,35 +3,76 @@ package com.shaheed.online29;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.animation.TranslateAnimation;
 
-import org.json.JSONObject;
+import com.shaheed.online29.game.GameEngine;
+import com.shaheed.online29.ws.WebSocketManager;
+
+import java.util.List;
+import java.util.Map;
 
 public class GameActivity extends Activity {
 
-    TextView textViewusername;
+    TextView tvStatus;
+    FrameLayout table;
+    GameEngine engine;
+    List<String> myHand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_game_table);
 
-        textViewusername = findViewById(R.id.gametextViewusername);
-        textViewusername.setText(Constants.username + " | Game: " + Constants.gameId);
+        tvStatus = findViewById(R.id.tvStatus);
+        table = findViewById(R.id.tableLayout);
 
-        // 1) Connect WebSocket
-        WSClient.getInstance().connect();
+        engine = new GameEngine();
+        engine.shuffle();
+        myHand = engine.dealHand(8);
 
-        // 2) Send joinRoom request
-        try {
-            JSONObject join = new JSONObject();
-            join.put("type", "joinRoom");
-            join.put("roomId", Constants.gameId);
-            join.put("playerName", Constants.username);
-            WSClient.getInstance().send(join);
+        showHand(myHand);
 
-        } catch (Exception e) {
-            Log.e("WS", e.toString());
+        WebSocketManager.get().setCallback(new WebSocketManager.Callback() {
+            @Override public void onOpen() {}
+            @Override public void onMessage(Map<String, Object> msg) {
+                runOnUiThread(() -> handleServerMessage(msg));
+            }
+            @Override public void onClose() {}
+            @Override public void onError(Throwable t) {}
+        });
+        // ensure connection (if not connected yet)
+        WebSocketManager.get().connect();
+    }
+
+    private void showHand(List<String> hand){
+        table.removeAllViews();
+        int x = 50;
+        for(String card : hand){
+            ImageView iv = new ImageView(this);
+            iv.setImageResource(R.drawable.card_back);
+            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(200,300);
+            lp.leftMargin = x;
+            lp.topMargin = 700;
+            table.addView(iv, lp);
+            x += 120;
+            TranslateAnimation anim = new TranslateAnimation(0,0, -500,0);
+            anim.setDuration(400);
+            iv.startAnimation(anim);
+        }
+    }
+
+    private void handleServerMessage(Map<String,Object> msg){
+        String type = (String) msg.get("type");
+        Log.i("GAME","msg: "+type);
+        if("deal".equals(type)){
+            tvStatus.setText("Dealt");
+        } else if("play".equals(type)){
+            tvStatus.setText("Play: "+msg.get("card"));
+        } else if("start".equals(type)){
+            tvStatus.setText("Game started");
         }
     }
 }
