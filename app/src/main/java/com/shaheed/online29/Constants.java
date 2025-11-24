2954b3a025c5626c1320d98ea0983c32d69aa99e
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-24 11:36:06 +0530

    Update Constants.java

--- app/src/main/java/com/shaheed/online29/Constants.java
+++ app/src/main/java/com/shaheed/online29/Constants.java
@@ -1,12 +1,10 @@
-public class Constants {
+package com.shaheed.online29;
 
+public class Constants {
+    // final websocket endpoint (you confirmed)
     public static final String WS_URL = "wss://two9onlinemultiplayer.onrender.com/ws/game";
 
-    public static String username;
-    public static int actionUserId = 0;
-
-    public static Boolean hostingGame = false;
-    public static Boolean joiningGame = false;
+    public static String username = "Guest";
     public static String gameId = "0";
-
+    public static int actionUserId = 0;
 }
