4ea95e6bab93b9d269161cc4af69a28b0f7a46e6
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-24 13:49:43 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,11 @@
 @echo off
-set DIR=%~dp0
-"%DIR%\gradle\wrapper\gradle-7.6\bin\gradle.bat" %*
+set APP_HOME=%~dp0
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+set JAVACMD=java.exe
+if defined JAVA_HOME (
+  set JAVACMD=%JAVA_HOME%\bin\java.exe
+)
+
+"%JAVACMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
